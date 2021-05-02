31354fe961eff3242d54f7ede1cf47243a9f5263
ridwan <irfanridwan20@gmail.com>
2021-05-02 16:14:15 +0600

    changed the entire content of homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,15 +1,22 @@
 function homepage(){
+    const body=document.querySelector('body');
     const content=document.querySelector('#content')
+    const parentdiv=document.createElement('div');
+    const div=document.createElement('div');
     const headerOne=document.createElement('h1');
-    const headerTwo=document.createElement('h2');
+    const headerTwo=document.createElement('P');
     const img=document.createElement('img');
-    headerOne.textContent="Tobago & Tripoli";
-    headerTwo.textContent="Where Bougie Meets Broke";
+    headerOne.textContent="A BOWL OF LOVE FROM JAPANESE COUSINE FOR YOU";
+    headerTwo.textContent="Honest ramen crafted with ethically farmed ingredients and love.";
     img.setAttribute('src','https://cdn.dribbble.com/users/4680578/screenshots/13882368/media/6fc181c5bb4ab9a3c57a5065cf36f107.jpeg?compress=1&resize=800x600');
-    
-    content.appendChild(headerOne)
-    content.appendChild(headerTwo)
-    content.appendChild(img)
+    parentdiv.style='display:flex;align-items:center'
+    img.style='float:right;height:100%;'
+    body.style.background='#ff2f2b'
+    div.appendChild(headerOne)
+    div.appendChild(headerTwo)
+    parentdiv.appendChild(div)
+    parentdiv.appendChild(img)
+    content.appendChild(parentdiv)
     return content;
 }
 export default homepage;
