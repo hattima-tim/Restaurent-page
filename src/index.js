49f0a02356b9721904e25b906db971b69833119f
ridwan <irfanridwan20@gmail.com>
2021-05-02 16:10:35 +0600

    wrote logic for tab browsing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('something')
+import homepage from './homepage';
+import menu from './menu'
+import contact from './contact'
+homepage();
+let parentDiv=document.querySelector('#content');
+let removePreviousTab=()=>{
+    while (parentDiv.firstChild) {
+        parentDiv.firstChild.remove()
+    }
+}
+let Home=document.querySelector('#home');
+let Menu=document.querySelector('#menu');
+let Contact=document.querySelector('#contact')
+Menu.addEventListener('click',()=>{
+    removePreviousTab();
+    menu()
+})
+Home.addEventListener('click',()=>{
+    removePreviousTab();
+    homepage()
+})
+Contact.addEventListener('click',()=>{
+    removePreviousTab();
+    contact()
+})
